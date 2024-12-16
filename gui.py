b3a15dd1eae596b0570c9575ff828a6c3fc280ca
lleejamiee <85136381+lleejamiee@users.noreply.github.com>
2024-12-17 12:46:34 +1300

    feat[graph]: Add text file data to graph

--- gui.py
+++ gui.py
@@ -4,30 +4,33 @@ import cv2
 from PIL import Image, ImageTk
 import threading
 import time
-import torch
+# import torch
 from ultralytics import YOLO
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 class VideoPlayer:
     percentage_output = ""
     file_name = ""
+
     def __init__(self, root):
         self.root = root
         self.root.title("Video Player with YOLO Object Detection")
-        
+
         self.left_canvas = Canvas(root, width=800, height=600)
         self.left_canvas.pack(side=tk.LEFT)
-        
+
         self.right_canvas = Canvas(root, width=800, height=600, bg='black')
         self.right_canvas.pack(side=tk.RIGHT)
-        
+
         self.btn_frame = tk.Frame(root)
         self.btn_frame.pack(fill=tk.X)
 
         # Add dropdown menus
         self.yolo_version_var = StringVar(value="YOLOv8")
         self.yolo_variant_var = StringVar(value="yolov8n")
-        
+
         self.yolo_versions = ["YOLOv8", "YOLOv9", "YOLOv10"]
         self.yolo_variants = {
             "YOLOv8": ["yolov8n", "yolov8s", "yolov8m", "yolov8l", "yolov8x"],
@@ -35,9 +38,10 @@ class VideoPlayer:
             "YOLOv10": ["YOLOv10-N", "YOLOv10-S", "YOLOv10-M", "YOLOv10-B", "YOLOv10-L", "YOLOv10-X"]
         }
 
-        self.yolo_version_menu = OptionMenu(self.btn_frame, self.yolo_version_var, *self.yolo_versions, command=self.update_yolo_variant_menu)
+        self.yolo_version_menu = OptionMenu(self.btn_frame, self.yolo_version_var, *self.yolo_versions,
+                                            command=self.update_yolo_variant_menu)
         self.yolo_version_menu.pack(side=tk.LEFT)
-        
+
         self.yolo_variant_menu = OptionMenu(self.btn_frame, self.yolo_variant_var, *self.yolo_variants["YOLOv8"])
         self.yolo_variant_menu.pack(side=tk.LEFT)
 
@@ -46,7 +50,7 @@ class VideoPlayer:
 
         self.btn_detect = tk.Button(self.btn_frame, text="Run", command=self.detect_objects)
         self.btn_detect.pack(side=tk.LEFT)
-        
+
         # Add the stop and reset button
         self.btn_stop_reset = tk.Button(self.btn_frame, text="X", command=self.stop_and_reset)
         self.btn_stop_reset.pack(side=tk.LEFT)
@@ -54,7 +58,7 @@ class VideoPlayer:
         # Add the text areas below the buttons
         self.text_area = Text(root, height=16, width=50)
         self.text_area.pack(fill=tk.X, padx=10, pady=5)
-        
+
         self.traffic_area = Text(root, height=16, width=50)
         self.traffic_area.pack(fill=tk.X, padx=10, pady=5)
 
@@ -62,22 +66,20 @@ class VideoPlayer:
         self.logging_area = Text(root, height=16, width=50)
         self.logging_area.pack(fill=tk.X, padx=10, pady=5)
 
-        
-
         self.cap = None
         self.frame = None
         self.running = False
         self.yolo = YOLO(self.get_yolo_model_path())  # Load initial YOLO model
         self.lock = threading.Lock()
-        
+
         # Define the classes to be detected
         self.allowed_classes = {0: 'person', 1: 'bicycle', 2: 'car', 3: 'motorbike', 5: 'bus', 7: 'truck'}
         self.black_frame = None
 
         self.left_fps = 0
         self.right_fps = 0
-        self.counter = 0   
-        self.start_time = time.time()     
+        self.counter = 0
+        self.start_time = time.time()
 
     def update_yolo_variant_menu(self, selected_version):
         self.yolo_variant_var.set(self.yolo_variants[selected_version][0])
@@ -88,7 +90,7 @@ class VideoPlayer:
     def get_yolo_model_path(self):
         version = self.yolo_version_var.get()
         variant = self.yolo_variant_var.get()
-        self.file_name = version + "_" + variant+".txt"
+        self.file_name = version + "_" + variant + ".txt"
         return f"{variant}.pt"
 
     def load_video(self):
@@ -98,11 +100,11 @@ class VideoPlayer:
             self.running = True
             self.play_video()
 
-    def play_video(self):        
+    def play_video(self):
         if self.cap.isOpened():
             with self.lock:
                 ret, frame = self.cap.read()
-                
+
                 if ret:
                     self.frame = frame
                     frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -127,22 +129,22 @@ class VideoPlayer:
         return cv2.resize(frame, (width, height), interpolation=cv2.INTER_AREA)
 
     def detect_objects(self):
-    
+
         self.start_time = time.time()
         if not self.running or self.frame is None:
             return
 
         # Reload the YOLO model with the selected version and variant
         self.yolo = YOLO(self.get_yolo_model_path())
-        
 
         def detection_thread():
-            
+
             while self.running:
                 start_time = time.time()
                 with self.lock:
                     ret, frame = self.cap.read()
                 if ret:
+                    # Drawing green boxes on the video. Passes in the current frame to yolo?
                     results = self.yolo(frame)
                     frame_with_boxes = self.draw_boxes(frame, results)
                     frame_with_boxes = cv2.cvtColor(frame_with_boxes, cv2.COLOR_BGR2RGB)
@@ -152,6 +154,7 @@ class VideoPlayer:
                     self.left_canvas.create_image(0, 0, anchor=tk.NW, image=imgtk_with_boxes)
                     self.left_canvas.imgtk = imgtk_with_boxes
 
+                    # Drawing grey boxes on the black box on the right
                     self.black_frame = self.draw_white_boxes(self.black_frame, results)
                     black_frame_with_boxes = cv2.cvtColor(self.black_frame, cv2.COLOR_BGR2RGB)
                     black_frame_with_boxes = self.resize_frame(black_frame_with_boxes, 800, 600)
@@ -160,32 +163,37 @@ class VideoPlayer:
                     self.right_canvas.create_image(0, 0, anchor=tk.NW, image=imgtk_black_with_boxes)
                     self.right_canvas.imgtk = imgtk_black_with_boxes
 
+                    # Writing frame per second in the black box
                     self.right_fps = 1.0 / (time.time() - start_time)
                     self.right_canvas.create_text(10, 10, anchor=tk.NW, text=f"FPS: {self.right_fps:.2f}", fill="white")
+                    # Writing Elapsed time
                     self.update_logging_area()
 
                     # Update the text area with the percentage of objects
                     percentageOfObj = self.calculate_percentage_of_obj(self.black_frame)
-                    self.percentage_output = self.percentage_output + str(percentageOfObj)+"\n"
+                    self.percentage_output = self.percentage_output + str(percentageOfObj) + "\n"
                     self.update_text_area(percentageOfObj)
-                    
-                time.sleep(0.01)	
-        
+
+                time.sleep(0.01)
+
         threading.Thread(target=detection_thread).start()
 
     def stop_and_reset(self):
         self.running = False
-        
-        print("save to"+self.file_name)
+
+        file_name = self.file_name
+        print("save to" + file_name)
         text_file = open(self.file_name, "w")
         text_file.write(self.percentage_output)
         text_file.close()
-        self.percentage_output=""
-        
+        self.percentage_output = ""
+
+        self.plot_gragh(file_name)
+
         if self.cap is not None:
             self.cap.release()
             self.cap = None
-                
+
         self.frame = None
         self.left_fps = 0
         self.right_fps = 0
@@ -200,6 +208,7 @@ class VideoPlayer:
         self.logging_area.delete('1.0', tk.END)
         self.right_canvas.configure(bg='black')
 
+    # results is from YOLO library, loops through all the objects that are detected
     def draw_boxes(self, frame, results):
         for result in results:
             for box in result.boxes:
@@ -209,8 +218,8 @@ class VideoPlayer:
                     label = self.allowed_classes[cls_id]
                     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                     cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-        return frame    
-    
+        return frame
+
     def draw_white_boxes(self, black_frame, results):
         for result in results:
             for box in result.boxes:
@@ -219,12 +228,12 @@ class VideoPlayer:
                     x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
                     x1, y1 = self.resize_coords(x1, y1, black_frame.shape[1], black_frame.shape[0])
                     x2, y2 = self.resize_coords(x2, y2, black_frame.shape[1], black_frame.shape[0])
-                    cv2.rectangle(black_frame, (x1, int((y1+y2)/2)), (x2, y2), (255, 255, 255), -1)
+                    cv2.rectangle(black_frame, (x1, int((y1 + y2) / 2)), (x2, y2), (255, 255, 255), -1)
         # Create a mask where the frame is greater than 0
-        mask = black_frame > 0        
-        
+        mask = black_frame > 0
+
         black_frame[mask] = np.log1p(black_frame[mask]) / np.log(1.05)
-        
+
         return black_frame
 
     def resize_coords(self, x, y, width, height):
@@ -242,15 +251,14 @@ class VideoPlayer:
         return percentageOfObj
 
     def update_text_area(self, percentageOfObj):
-        #self.text_area.delete('1.0', tk.END)
+        # self.text_area.delete('1.0', tk.END)
         self.text_area.insert(tk.END, f"Percentage of Road Coverage: \t{percentageOfObj:.2f}\n")
         self.text_area.see(tk.END)
-        
-        #self.traffic_area.delete('1.0', tk.END)
+
+        # self.traffic_area.delete('1.0', tk.END)
         traffic_description = self.get_traffic_description(percentageOfObj)
         self.traffic_area.insert(tk.END, f"Traffic description: \t{traffic_description}\n")
         self.traffic_area.see(tk.END)
-        
 
     def get_traffic_description(self, percentageOfObj):
         if percentageOfObj <= 1:
@@ -268,19 +276,44 @@ class VideoPlayer:
         else:
             return "Congested."
 
-    def update_logging_area(self):        
+    def update_logging_area(self):
         elapsed_time = time.time() - self.start_time
-        self.percentage_output = self.percentage_output + str(elapsed_time)+", "
-        log_message = f"Elapsed time (s): \t{elapsed_time:.2f}"        
+        self.percentage_output = self.percentage_output + str(elapsed_time) + ", "
+        log_message = f"Elapsed time (s): \t{elapsed_time:.2f}"
         self.logging_area.insert(tk.END, log_message + '\n')
         self.logging_area.see(tk.END)
 
     def on_closing(self):
-                
+
         self.running = False
         if self.cap is not None:
             self.cap.release()
         self.root.destroy()
+
+    def plot_gragh(self, file_name):
+        times = []
+        coverage = []
+        with open(file_name, 'r') as file:
+            for line in file:
+                parts = line.strip().split(',')
+                if len(parts) == 2:  # Check if line has exactly two elements
+                    try:
+                        current_time, percent = map(float, parts)
+                        times.append(current_time)
+                        coverage.append(percent)
+                    except ValueError:
+                        print(f"Skipping invalid line: {line.strip()}")
+                else:
+                    print(f"Skipping line with unexpected format: {line.strip()}")
+
+        plt.figure(figsize=(10, 5))
+        plt.plot(times, coverage, color='black', linestyle='-')
+        plt.xlabel('Time (ms)')
+        plt.ylabel('Percentage of Road Coverage')
+        plt.title('Percentage of Road Coverage')
+        plt.grid(True)
+        plt.show()
+
 
 if __name__ == "__main__":
     root = tk.Tk()
